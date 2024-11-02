027093bb8794026439cb31ab8f65e7093d66e86d
Gabriel Pacheco <gabrielfppacheco@gmail.com>
2024-11-02 14:29:48 -0300

    Criação do novo portfolio

---
+++ portfolio/app/components/Hero.tsx
@@ -0,0 +1,24 @@
+import "./style.css";
+const Hero = () => {
+  const textGradient =
+    "bg-gradient-to-br from-teal-100 via-teal-200 to-teal-500 text-transparent bg-clip-text";
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-center text-white">
+        <h1 className={`text-5xl font-bold ${textGradient} animate-slide-in`}>
+          Hi, I'm Gabriel!{" "}
+          <span className="inline-block animate-wave text-white emoji-white">
+            👋
+          </span>
+        </h1>
+
+        <h2 className="mt-4 text-3xl animate-fade-in">
+          Welcome to my portfolio!
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
